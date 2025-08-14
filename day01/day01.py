668d229799a5fe7fd5efadddf66493a7f7a00bd9
Bianca Danforth <biancadanforth@gmail.com>
2025-08-14 11:50:08 -0400

    Add initial day01 test and source code skeleton
    The tests currently pass when running 'pytest -f day01' from the root directory.

---
+++ day01/day01.py
@@ -0,0 +1,11 @@
+class Puzzle():
+    @classmethod
+    def solve(cls, puzzle_input):
+        return 1
+
+def main():
+    puzzle_input = "test"
+    Puzzle.solve(puzzle_input)
+
+if __name__ == "__main__":
+    main()
